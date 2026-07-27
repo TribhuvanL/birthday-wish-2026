110b31fd385bc7f6db6368cbeb4c20b4465adf13
Tribhuvan Yeshraj <tribhuvanl892@gmail.com>
2026-07-27 22:09:57 +0530

    Add login functionality and photo display logic

---
+++ script.js
@@ -0,0 +1,91 @@
+const USERNAME = "madhushree";
+const PASSWORD = "tridhu2817";
+
+const photos = [
+
+"photos/1.jpg",
+"photos/2.jpg",
+"photos/3.jpg",
+"photos/4.jpg",
+"photos/5.jpg",
+"photos/6.jpg",
+"photos/7.jpg",
+"photos/8.jpg",
+"photos/9.jpg",
+"photos/10.jpg"
+
+];
+
+function login(){
+
+const user=document.getElementById("username").value;
+const pass=document.getElementById("password").value;
+
+if(user===USERNAME && pass===PASSWORD){
+
+document.getElementById("loginPage").style.opacity=0;
+
+setTimeout(()=>{
+
+document.getElementById("loginPage").style.display="none";
+document.getElementById("mainPage").style.display="block";
+
+startPhotos();
+
+},1000);
+
+}
+else{
+
+const popup=document.getElementById("popup");
+
+popup.style.opacity=1;
+
+setTimeout(()=>{
+
+popup.style.opacity=0;
+
+},2500);
+
+}
+
+}
+
+function random(min,max){
+
+return Math.random()*(max-min)+min;
+
+}
+
+function createPhoto(){
+
+const img=document.createElement("img");
+
+img.src=photos[Math.floor(Math.random()*photos.length)];
+
+img.className="photo";
+
+img.style.left=random(5,80)+"%";
+img.style.top=random(5,70)+"%";
+
+const move=Math.random()<0.5?-350:350;
+
+img.style.setProperty("--moveX",move+"px");
+
+img.style.setProperty("--rotate",random(-18,18)+"deg");
+
+document.getElementById("photoContainer").appendChild(img);
+
+setTimeout(()=>{
+
+img.remove();
+
+},8000);
+
+}
+
+function startPhotos(){
+
+setInterval(createPhoto,1200);
+
+}
